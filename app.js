e8f27247fba61ec82638a678746485366077f0b8
Faizan01851 <fezik1547@gmail.com>
2025-09-03 09:18:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// // script.js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuBtn = document.getElementById("menu-btn");
+//     const navLinks = document.getElementById("nav-links");
+
+//     menuBtn.addEventListener("click", function () {
+//         navLinks.classList.toggle("open");
+//     });
+// });
+
+
+
+
+document.addEventListener("DOMContentLoaded" ,function(){
+    const menuBtn = document.getElementById("menu-btn");
+    const navLinks = document.getElementById("nav-links");
+
+
+    menuBtn.addEventListener("click" , function(){
+        navLinks.classList.toggle("open");
+    })
+})
